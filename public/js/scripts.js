9637642598b5737774812bab13eaa62ee1a18602
Sam Matthews <sam@subsupps.co.nz>
2019-07-23 18:36:13 +1200

    sets up Express and tests

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+//require express
+const express = require('express');
+//create an express application
+const app = express();
+//set the app to listen to port 3000
+app.listen(3000);
+//testing the route
+app.get('/', (req, res, next) =>{
+    res.send('Express is operational!');
+});
