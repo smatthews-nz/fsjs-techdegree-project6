0b84e9e948a956927fbd46aa32d4dc3c5091fef4
Sam Matthews <sam@subsupps.co.nz>
2019-07-23 20:12:06 +1200

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,1 @@
-//require express
-const express = require('express');
-//create an express application
-const app = express();
-//set the app to listen to port 3000
-app.listen(3000);
-//testing the route
-app.get('/', (req, res, next) =>{
-    res.send('Express is operational!');
-});
+$(document).foundation()
