aa6ca46a563136d16f42d5f6d4f2b516c97ecb45
Sam Matthews <sam@subsupps.co.nz>
2019-07-23 20:15:00 +1200

    add routing for index

---
+++ app.js
@@ -0,0 +1,20 @@
+//require express
+const express = require('express');
+//create an express application
+const app = express();
+
+//set the view engine to pug
+app.set('view engine', 'pug');
+
+//serve static assets
+app.use('/static', express.static('public'));
+
+//setting the route for index
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+//set the app to listen to port 3000
+app.listen(3000, () => {
+    console.log('Project is running on port 3000');
+});
